cd8ad9f9bf1dfa74a739644c128674ad54be26d6
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2023-04-22 21:49:28 +0200

    modal ok

--- src/app/core/components/modal/modal.component.ts
+++ src/app/core/components/modal/modal.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { ModalService } from '../../services/modal/modal.service';
+import { GastosService } from '../../services/gastos/gastos.service';
+import { IngresosService } from '../../services/ingresos/ingresos.service';
+import { IGasto } from '../../services/models/gastos.models';
+import { Iingreso } from '../../services/models/ingreso.models';
 
 @Component({
   selector: 'app-modal',
@@ -7,9 +11,69 @@ import { ModalService } from '../../services/modal/modal.service';
   styleUrls: ['./modal.component.css']
 })
 export class ModalComponent {
+  /**
+   *Almacenamos los gastos en un array vacio
+    */
+   gastos: IGasto[]=[]
+   /**
+    * 
+    * Almacenamos los ingresos en un array vacio  
+    */
+   ingresos:Iingreso[]=[]
+   /**
+    *flag para guardar la suma  
+    */ 
+     sumaG:number=0 ;
+     /**
+    *flag para guardar la suma  
+    */ 
+    sumaIngreso:number=0 ;
+    /**
+     * flag para guardar la diferencia
+     */
+    diferencia:number=0;
+    gastoMayor:boolean=false;
+    ingresoMayor:boolean=false;
   constructor(
-    private modalService: ModalService
+    private modalService: ModalService,
+    private gastosService: GastosService,
+    private ingresosService: IngresosService,
   ) { }
+  ngOnInit(): void {
+    this.getTotalGastos();
+    this.getTotalIngresos();
+    // this.calcularGastoMayor();
+    
+  }
+  private getTotalGastos() {
+    this.gastosService.getGastos().subscribe((gastos) => {
+      this.gastos = gastos;
+      let suma = 0;
+      for (let i = 0; i < this.gastos.length; i++) {
+        suma += this.gastos[i].importe;
+      }
+    this.sumaG = suma;
+    });
+  }
+  private getTotalIngresos() {
+    this.ingresosService.getIngreso().subscribe((ingresos) => {
+      this.ingresos = ingresos;
+      let suma = 0;
+      for (let i = 0; i < this.ingresos.length; i++) {
+        suma += this.ingresos[i].importe;
+      }
+    this.sumaIngreso = suma;
+    console.log(this.sumaIngreso,60)
+    });
+  }
+  calcularDiferencia(){
+    if(this.sumaIngreso > this.sumaG){
+      this.diferencia= this.sumaIngreso - this.sumaG
+    }else{
+      this.diferencia= this.sumaIngreso - this.sumaG
+    }
+    return this.diferencia;
+  }
 
   closeModal() {
     this.modalService.closeModal();
--- src/app/core/components/panel-control/panel-control.module.ts
+++ src/app/core/components/panel-control/panel-control.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
-
 // import { PanelControlComponent } from './panel-control.component';
 
 
