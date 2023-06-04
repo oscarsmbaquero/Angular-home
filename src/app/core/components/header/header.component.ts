1b6fd92708b3ed163fb8441eb698550bb42fa8f7
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2023-06-04 21:03:32 +0200

    header with tareas

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TareasService } from '../../services/tareas/tareas.service';
+import { ITareas } from '../../services/models/tareas.models';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+ /**
+  * flag almacenar las tareas
+  */
+  numeroTareas:number=0;
+
+  constructor(
+    private tareasService:TareasService
+  ){ }
+
+  ngOnInit(){
+    console.log('Entro')
+    this.getNumeroTareas();
+  }
+
+  private getNumeroTareas() {
+    console.log('Entro')
+    const mesActual = new Date().getMonth(); // Obtenemos el mes actual (0-11)
+  
+    this.tareasService.getTareas().subscribe((tareas) => {
+      const tareasFiltradas = tareas.filter((tarea) => {
+        const mesTarea = new Date(tarea.fecha).getMonth();
+        return mesTarea === mesActual;
+      });
+  
+      this.numeroTareas = tareasFiltradas.length;
+      console.log('Número de tareas en el mes actual:',this.numeroTareas);
+      // O haz algo más con el número de tareas filtradas
+    });
+  }
+
   isCollapse = true;
   toggleState() {
     let foo = this.isCollapse
